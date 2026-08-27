86fe0a261ee4aff3f04d7a4087ab9c5fab4c4415
Artyom Gavrilov <temagawr2012@gmail.com>
2026-08-27 23:01:17 +0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
 // Вставьте URL веб-приложения Google Apps Script после деплоя.
 // Пример: "https://script.google.com/macros/s/XXXX/exec"
-const SCRIPT_URL = "";
+const SCRIPT_URL =
+  "https://script.google.com/macros/s/AKfycbyJVh7dFNC-ceelruwoZJdI4tBeiQBLmPgFwkGuK4DXghj81PrRVYNuhS11QmjUzMW2Hg/exec";
